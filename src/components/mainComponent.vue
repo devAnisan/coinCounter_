8e0de2afe4ad3d6cf5c221b928d21904a88d5f25
devAnisan <animan667@gmail.com>
2025-08-22 18:40:49 -0600

    add good design, buttom, box shadow and center the items

--- src/components/mainComponent.vue
+++ src/components/mainComponent.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="mainCounter" class="flex flex-col m-1">
-    <div class="flex gap-2 justify-between">
+    <div class="flex gap-2 justify-center items-center">
       <h1 class="text-3xl font-bold">Contador de monedas</h1>
       <button
-        class="border p-2 font-bold rounded-sm bg-gray-300 hover:bg-gray-400 focus:cursor-pointer"
+        class="border-none p-3.5 font-bold text-amber-50 rounded-sm bg-red-400 hover:bg-red-500 hover:cursor-pointer"
         v-on:click="cleaner"
       >
         Limpiar
       </button>
     </div>
-    <div v-for="(coin, index) in coins" :key="coin.valor" class="flex gap-3 items-center py-0">
-      <h3 class="text-left p-1 w-16">{{ coin.valor }}</h3>
-      <input
-        v-model="coin.cantidad"
-        class="p-1 w-20"
-        type="number"
-        @keydown.enter.prevent="focusNext(index)"
-      />
-      =
-      <h1 class="w-52">{{ coin.valor * coin.cantidad }}</h1>
+    <div class="shadow-md p-8 m-4 rounded-md w-2xl flex flex-col justify-center items-center">
+      <div
+        v-for="(coin, index) in coins"
+        :key="coin.valor"
+        class="flex w-80 justify-between items-center py-0"
+      >
+        <h3 class="text-left p-1 w-16">{{ coin.valor }}</h3>
+        <input
+          v-model="coin.cantidad"
+          class="p-1 w-20"
+          type="number"
+          @keydown.enter.prevent="focusNext(index)"
+        />
+        =
+        <h1>{{ coin.valor * coin.cantidad }}</h1>
+      </div>
     </div>
     <div class="text-2xl font-bold">Total {{ totalFormat }}</div>
   </div>
